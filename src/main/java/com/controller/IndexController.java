4e5646fe668f17be37f18998da605ab4f2acb54b
jiangchao08 <jiangchao08@baidu.com>
2017-02-19 21:49:32 +0800

    修复打jar包的bug，同时修改端口号9000

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -15,44 +15,43 @@ public class IndexController {
 
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index(){
-        return "/front/index";
+        return "front/index";
     }
 
     @RequestMapping(value = "/list/{page}",method = RequestMethod.GET)
     public String list(@PathVariable("page") Integer page, ModelMap modelMap){
         System.out.println("page="+page);
         modelMap.addAttribute("a","123");
-        return "/front/commodityList";
+        return "front/commodityList";
     }
 
     @RequestMapping(value = "/new",method = RequestMethod.GET)
     public String add(){
-        return "/front/new";
+        return "front/new";
     }
 
     @RequestMapping(value = "/contactus",method = RequestMethod.GET)
     public String contactus(){
-
-        return "/front/contactus";
+        return "front/contactus";
     }
 
     @RequestMapping(value = "/view",method = RequestMethod.GET)
     public String view(){
-        return "/front/view";
+        return "front/view";
     }
 
     @RequestMapping(value = "/product_detail",method = RequestMethod.GET)
     public String product_detail(){
-        return "/front/product_detail";
+        return "front/product_detail";
     }
 
     @RequestMapping(value = "/products",method = RequestMethod.GET)
     public String products(){
-        return "/front/products";
+        return "front/products";
     }
 
     @RequestMapping(value = "/project_detail",method = RequestMethod.GET)
     public String pricing_tables(){
-        return "/project_detail";
+        return "project_detail";
     }
 }
